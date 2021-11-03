d2c5ca3de55e224a936efa0a6c32624bbdc5c5c0
TJHL <tjhl8@DESKTOP-4DHENL0>
2021-11-02 19:58:17 -0700

    YoU MUst ENTEr A coMItT mesSAGE

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -14,12 +14,35 @@ public class Validation {
 		
 		int randomNumber = randomMaker.nextInt(5);
 		
-		System.out.println(randomNumber);
-
-		// 1. Use each value of randomNumber to give the user a random compliment.
-
-		// 2. Repeat all the code above 10 times
+		for (int i = 1; i < 10; i ++) {
+			randomNumber = randomMaker.nextInt (5);
+			System.out.println(randomNumber);
 		
+		//System.out.println(randomNumber);
+
+	//	}
+		// 1. Use each value of randomNumber to give the user a random compliment.
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "trash");
+		}
+		if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "bad");
+		}
+		if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "loser");
+		}
+		if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "idiot");
+		}
+		if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "stupid");
+		}
+		// 2. Repeat all the code above 10 times
+		//for (int i = 1; i < 10; i ++) {
+			//randomNumber = randomMaker.nextInt (5);
+			//System.out.println(randomNumber);
+		}
+		JOptionPane.showMessageDialog(null, "You get no validation from me stupid!!!");
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
