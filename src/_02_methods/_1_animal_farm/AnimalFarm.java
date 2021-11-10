64c083f24693dba09cdcf2b2514ae374684ca14d
TJHL <tjhl8@DESKTOP-4DHENL0>
2021-11-09 19:58:13 -0800

    Commit message

--- src/_02_methods/_1_animal_farm/AnimalFarm.java
+++ src/_02_methods/_1_animal_farm/AnimalFarm.java
@@ -28,9 +28,9 @@ public class AnimalFarm {
 		/* 1. Ask the user which animal they want, then see and hear 
 		 *    the animal they chose using one of the methods below.
 		*/			 
-			
+			String animal = JOptionPane.showInputDialog(null,"Pick a farm animal: A cow, duck, dog, cat, or a llama");
 		/* 2. Make it so that the user can keep entering new animals. */
-
+			
 	}
 
 	void moo() {
